9c512292d4ee8a749d3942e55937cd3db1dc9819
Nuno Cordeiro <nuno.cordeiro@inm.pt>
2023-06-06 17:39:24 +0100

    modals

--- playground/ContentView.swift
+++ playground/ContentView.swift
@@ -10,71 +10,33 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var showSheet = false
-    
+  @State var showInfoModalView: Bool = false
+  
+  var body: some View {
+    VStack(spacing: 50) {
+      Text("Main View")
+        .font(.largeTitle)
+      
+      Button(action: {
+        showInfoModalView = true
+      }, label: {
+        Label("Show Info View", systemImage: "info.circle")
+      })
+    }
+    .sheet(isPresented: $showInfoModalView) {
+        InfoView()
+    }.frame(maxHeight: 200)
+  }
+}
+
+
+struct InfoView: View {
     var body: some View {
-        VStack {
-            Button("The new Button") {
-                showSheet = true
-            }
-            .actionSheet(isPresented: $showSheet, content: {
-                ActionSheet(title: Text("My title"),
-                            message: Text("kindly note this is a message"),
-                            buttons: [])
-            }).ignoresSafeArea()
-            Button("Ana") {
-                
-            }
+        VStack(spacing: 50)  {
+            Text("Nuno")
         }
     }
 }
-
-
-
-// TODO: Add HTML Tags later.
-struct BottomSheetNotificationView: View {
-    enum Content: Equatable {
-        case `default`(title: String, description: String)
-        case large(description: String)
-    }
-
-    let content: Content
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 5) {
-            switch content {
-                case .default(title: let title, description: let description):
-                    createDefaultStyledContent(title: title, description: description)
-                case .large(description: let description):
-                    createLargeStyledContent(description: description)
-            }
-        }
-        .padding([.leading, .trailing], 20)
-    }
-}
-
-private extension BottomSheetNotificationView {
-    func createDefaultStyledContent(title: String, description: String) -> some View {
-        VStack(alignment: .leading, spacing: 16) {
-            WeChargeUIComponents.Text.attributedHeader(title, fontSize: 24, fontWeight: .light)
-                .foregroundColor(.black000)
-
-            WeChargeUIComponents.Text.attributedText(description, fontSize: 16)
-                .foregroundColor(.black000)
-        }
-    }
-
-    func createLargeStyledContent(description: String) -> some View {
-        WeChargeUIComponents.Text.attributedHeader(description, fontSize: 24, fontWeight: .light)
-            .foregroundColor(.black000)
-    }
-}
-
-
-
-
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
