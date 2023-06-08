d4e4164aa6249eec82b7686d97832d986f1e8be2
Nuno Cordeiro <nunocordeiro@hotmail.com>
2023-06-08 01:02:35 +0100

    popUp with delegate working

--- playground/ContentView.swift
+++ playground/ContentView.swift
@@ -8,42 +8,110 @@ import Foundation
 import SwiftUI
 
 
-
 struct ContentView: View {
-  @State var showInfoModalView: Bool = false
-  
-  var body: some View {
-    VStack(spacing: 50) {
-      Text("Main View")
-        .font(.largeTitle)
-      
-      Button(action: {
-        showInfoModalView = true
-      }, label: {
-        Label("Show Info View", systemImage: "info.circle")
-      })
-    }
-    .sheet(isPresented: $showInfoModalView) {
-        InfoView()
-    }.frame(maxHeight: 200)
-  }
-}
-
-
-struct InfoView: View {
+    
+    @State var isShowingBottomSheet: Bool = false
+    
     var body: some View {
-        VStack(spacing: 50)  {
-            Text("Nuno")
+        ZStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+                Button {
+                    isShowingBottomSheet = true
+                } label: {
+                    Text("Show bottom sheet")
+                }.buttonStyle(.borderedProminent)
+            }
+            let buttons: [newButton] =
+                [.init(label: "Nuno", customAction: { print("Hello Wooorld!!!") })]
+            BottomSheetView(title: "This is my title", description: "Long description of the popUp goes here. You can customize this text", isShowing: $isShowingBottomSheet, buttons: buttons)
         }
     }
 }
+
+
+protocol newButtonDelegate {
+    func didTapped()
+}
+
+struct newButton: View, Identifiable {
+    let id = UUID()
+    var label: String
+    var customAction: (()->Void)
+    var delegate: newButtonDelegate?
+    
+    var body: some View {
+        Button {
+            customAction()
+            delegate?.didTapped()
+        } label: {
+            Text(label)
+                .frame(maxWidth: .infinity)
+        }
+        .buttonStyle(.borderedProminent)
+        .clipShape(Capsule())
+    }
+}
+
+
+struct BottomSheetView: View, newButtonDelegate {
+
+    let title: String
+    let description: String
+    
+    @Binding var isShowing: Bool
+    var buttons = [newButton]()
+    
+    
+    var body: some View {
+        
+        ZStack(alignment: .bottom) {
+            
+            if isShowing {
+                Color.black
+                    .opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation {
+                            isShowing = false
+                        }
+                    }
+                VStack {
+                    Text(title).font(.title).padding()
+                    Text(description)
+                    ForEach(buttons) { button in
+                        newButton(label: button.label,
+                                         customAction: button.customAction,
+                                         delegate: self)
+                        .padding(.top)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, 50)
+                .padding(.leading, 30)
+                .padding(.trailing, 30)
+                .background(.white)
+                .cornerRadius(16)
+                .transition(.opacity.combined(with: .move(edge: .bottom)))
+            }
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
+        .animation(.easeInOut, value: isShowing)
+    }
+
+    func didTapped() {
+        isShowing = false
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
-
-
-
 
